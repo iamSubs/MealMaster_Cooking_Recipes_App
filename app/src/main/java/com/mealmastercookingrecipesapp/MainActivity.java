e5306f9993ec91e6151befd6af729b62703f7a62
iamSubs <artem-votteler@hotmail.de>
2023-04-20 16:33:05 +0200

    Api BottomNavBar

--- app/src/main/java/com/mealmastercookingrecipesapp/MainActivity.java
+++ app/src/main/java/com/mealmastercookingrecipesapp/MainActivity.java
@@ -6,19 +6,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.mealmastercookingrecipesapp.databinding.ActivityMainBinding;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+    String url;
+
+    Button loadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new HomeFragment());
+        replaceFragment(new SearchFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
@@ -39,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+
+
+        url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=77e59043d435499d85a39c0915fdd41f&query=chicken";
+        loadButton = (Button) findViewById(R.id.loadButton);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String text = response.getString("results");
+
+                }catch (Exception e){
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
 
     }
 
